c09141b4acb5b5ae4f08d7fb6cd0ec27dfbacdb5
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-19 14:00:15 -0300

    feat: basic functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("oi")
+import { writeFile } from "fs/promises";
+import puppeteer from 'puppeteer';
+import { formatHTML, Product } from "./html.js";
+
+const product: Product = {
+  id: "XYZZZ",
+  title: "Apple iPhone 14 (128 GB) – Roxo",
+  imageUrl: "https://m.media-amazon.com/images/I/41Z8j2+aznL._AC_SL1000_.jpg",
+  priceOld: "7599.90",
+  priceNew: "5499.00",
+  priceDiscount: "28",
+  acceptInstallments: true,
+  freeShipping: true
+}
+
+const filePath = `file:///home/gmassoqueto/github-repos/node-html-to-image/${product.id}.html`
+await writeFile(`${product.id}.html`, formatHTML(product))
+
+const browser = await puppeteer.launch({ headless: "new" });
+const page = await browser.newPage();
+await page.setViewport({
+  width: 1080,
+  height: 1920,
+  deviceScaleFactor: 2,
+});
+await page.goto(filePath)
+await page.screenshot({ path: 'screenshot.webp', type: 'webp', quality: 100 })
+await browser.close()
