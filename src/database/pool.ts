23dbf135c72e83c1b7ebaffb719f4d3882d53410
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-19 16:19:05 -0300

    feat: add database pool

---
+++ src/database/pool.ts
@@ -0,0 +1,14 @@
+import pkg from 'pg';
+import { POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER} from '../settings.js'
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: POSTGRES_HOST,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DB,
+  port: POSTGRES_PORT,
+  max: 20,
+  idleTimeoutMillis: 10,
+  connectionTimeoutMillis: 1000,
+})
