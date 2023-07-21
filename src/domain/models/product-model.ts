46f57fdbfedcb929652642ab4504b77e23875780
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-21 13:25:00 -0300

    feat: add product model

--- src/domain/models/product-model.ts
+++ src/domain/models/product-model.ts
@@ -1,10 +1,12 @@
 export type ProductModel = {
   id: string
   title: string
+  image_url: string
   category: string
   reviews: number
-  is_prime: boolean
-  price: number
+  free_shipping: boolean
+  price: string
+  previous_price: string
   discount: number
   created_at: Date
   updated_at: Date
