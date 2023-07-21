5df56fbb02a83f35e19c7a93bbf0b665862dd38d
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-21 13:24:37 -0300

    chore: remove environment variable

--- src/settings.ts
+++ src/settings.ts
@@ -5,8 +5,9 @@ export const POSTGRES_DB = process.env.POSTGRES_DB ?? "postgres"
 export const POSTGRES_USER = process.env.POSTGRES_USER ?? "postgres"
 export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD ?? "password"
 export const POSTGRES_HOST = process.env.POSTGRES_HOST ?? "localhost"
-export const TEMP_FOLDER = process.env.TEMP_FOLDER!
+
 export const SCREENSHOTS_FOLDER = process.env.SCREENSHOTS_FOLDER ?? "./screenshots"
+
 export const RABBITMQ_DEFAULT_USER = process.env.RABBITMQ_DEFAULT_USER ?? "user"
 export const RABBITMQ_DEFAULT_PASS = process.env.RABBITMQ_DEFAULT_PASS ?? "password"
 export const RABBITMQ_DEFAULT_HOST = process.env.RABBITMQ_DEFAULT_HOST ?? "localhost"
