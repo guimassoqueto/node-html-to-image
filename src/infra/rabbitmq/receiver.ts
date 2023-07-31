e6838aef3d49a379cf90c1f0f69d85b4ad6fe051
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-31 15:36:53 -0300

    chore: update imports

--- src/infra/rabbitmq/receiver.ts
+++ src/infra/rabbitmq/receiver.ts
@@ -1,7 +1,7 @@
 import client, { Connection, Channel, ConsumeMessage } from 'amqplib'
 import { 
   RABBITMQ_URL,
-  RABBITMQ_MAIN_QUEUE,
+  RABBITMQ_RECEIVER_QUEUE,
 } from '../../settings.js'
 
 export type TConsumer = (channel: Channel) => (message: ConsumeMessage | null) => Promise<void>
@@ -10,7 +10,7 @@ export class RabbitMQReceiver {
   static async receiver(message_handler: TConsumer) {
     const connection: Connection = await client.connect(RABBITMQ_URL)
     const channel: Channel = await connection.createChannel()
-    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { durable: false })
-    await channel.consume(RABBITMQ_MAIN_QUEUE, message_handler(channel), { noAck: false })
+    await channel.assertQueue(RABBITMQ_RECEIVER_QUEUE, { durable: false })
+    await channel.consume(RABBITMQ_RECEIVER_QUEUE, message_handler(channel), { noAck: false })
   }
 }
