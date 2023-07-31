faaba13d2de93aedcc394671133498b2aa2a7e92
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-31 15:37:25 -0300

    refactor: update screenshot service

--- src/services/screenshot-service.ts
+++ src/services/screenshot-service.ts
@@ -5,12 +5,13 @@ import { Product } from "../types/product.js";
 
 export class ScreenshotService {
   static async takeShot(product: Product): Promise<void> {
+    const pageHTML = await createHTMLString(product)
     const browser = await puppeteer.launch({ headless: "new" })
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1920 })
-    await page.setContent(createHTMLString(product), { waitUntil: 'networkidle2' })
+    await page.setContent(pageHTML, { waitUntil: 'networkidle2' })
     await page.screenshot({
-      path: `${SCREENSHOTS_FOLDER}/${product.id}.webp`,
+      path: `${SCREENSHOTS_FOLDER}/${product.id.slice(-10)}.webp`,
       type: "webp",
       quality: 100,
     });
