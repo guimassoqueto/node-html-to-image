b096349189e8e5fcbc250a0a363b02c28b555621
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-07 14:56:53 -0300

    feat: update kadec screenshots

--- src/utils/kadec-shots.ts
+++ src/utils/kadec-shots.ts
@@ -1,18 +1,17 @@
 import pLimit from "p-limit";
 import { PostgresRepository } from "../infra/postgres/postgres-repository.js";
-import { kadecQuery } from "../infra/postgres/queries/queries.js";
 import { Screenshot } from "../screenshoter/screenshot.js";
 import { KADEC_SCREENSHOTS_FOLDER } from "../settings.js";
 import { KadecTemplater } from "../templaters/kadec/kadec.js";
 
 
-export default async function kadecShots(): Promise<void> {
+export default async function thunderShots(query: string): Promise<void> {
   const limit = pLimit(3);
 
   let promises: Promise<void>[] = [];
 
-  const kadecProducts = await PostgresRepository.loadProducts(kadecQuery);
-  for (const product of kadecProducts) {
+  const thunderProducts = await PostgresRepository.loadProducts(query);
+  for (const product of thunderProducts) {
     const template = KadecTemplater.generate(product);
     promises.push(
       limit(() =>
@@ -21,5 +20,5 @@ export default async function kadecShots(): Promise<void> {
     );
   }
 
-  await Promise.all(promises)
+  await Promise.all(promises);
 }
