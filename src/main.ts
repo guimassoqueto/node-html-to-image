460a7dcccd0e41f3db51a718eb7a25adcc6d7b39
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-07 14:58:05 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
+import { queryByAmazonIds } from "./infra/postgres/queries/queries.js";
 import { Consumer, RabbitMQReceiver } from "./infra/rabbitmq/receiver.js";
 import kadecShots from "./utils/kadec-shots.js";
 import thunderShots from "./utils/thunder-shots.js";
 
+type JsonMessage = {
+  kadec?: string[]
+  thunder?: string[]
+}
+
 const messageHandler: Consumer = (channel) => {
   return async (message) => {
     if (message) {
-      console.log(`Message Received: ${message}`)
-      await takeShots()
+      console.log(`Message Received`)
+      const jsonMessage = JSON.parse(message.content.toString()) as JsonMessage
+
+      await takeShots(jsonMessage)
+
       channel.ack(message);
       console.log("Waiting for new messages...")
     }
   }
 }
 
-async function takeShots(): Promise<void> {
-  await thunderShots()
-  await kadecShots()
+async function takeShots(jsonMessage: JsonMessage): Promise<void> {
+  if (jsonMessage.kadec) await kadecShots(queryByAmazonIds(jsonMessage.kadec))
+  if (jsonMessage.thunder) await thunderShots(queryByAmazonIds(jsonMessage.thunder))
 }
 
 try {
