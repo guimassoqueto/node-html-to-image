ba62fe86ba6098957daabbca0277a0103d94d581
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-07 14:57:41 -0300

    feat: update rabbitmq receiver

--- src/infra/rabbitmq/receiver.ts
+++ src/infra/rabbitmq/receiver.ts
@@ -1,16 +1,17 @@
-import client, { Connection, Channel, ConsumeMessage } from 'amqplib'
-import { 
-  RABBITMQ_URL,
-  RABBITMQ_RECEIVER_QUEUE,
-} from '../../settings.js'
+import client, { Channel, Connection, ConsumeMessage } from "amqplib";
+import { RABBITMQ_RECEIVE_QUEUE, RABBITMQ_URL } from "../../settings.js";
 
-export type Consumer = (channel: Channel) => (message: ConsumeMessage | null) => Promise<void>
+export type Consumer = (
+  channel: Channel,
+) => (message: ConsumeMessage | null) => Promise<void>;
 
 export class RabbitMQReceiver {
   static async receiver(message_handler: Consumer) {
-    const connection: Connection = await client.connect(RABBITMQ_URL)
-    const channel: Channel = await connection.createChannel()
-    await channel.assertQueue(RABBITMQ_RECEIVER_QUEUE, { durable: false })
-    await channel.consume(RABBITMQ_RECEIVER_QUEUE, message_handler(channel), { noAck: false })
+    const connection: Connection = await client.connect(RABBITMQ_URL);
+    const channel: Channel = await connection.createChannel();
+    await channel.assertQueue(RABBITMQ_RECEIVE_QUEUE, { durable: false });
+    await channel.consume(RABBITMQ_RECEIVE_QUEUE, message_handler(channel), {
+      noAck: false,
+    });
   }
 }
